f0da5a238ab2c98d367c44639196d88ae5507ba8
siri2007-vennela <vtu29536@veltech.edu.in>
2026-09-07 21:52:44 +0530

    Time: 1 ms (100%), Space: 43.8 MB (39.75%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,36 @@
+
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0;
+        int n = s.length();
+        int sign = 1;
+        long result = 0;
+
+        // Skip leading spaces
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        // Check sign
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-')
+                sign = -1;
+            i++;
+        }
+
+        // Convert digits
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+
+            if (sign == 1 && result > Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+
+            if (sign == -1 && -result < Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+
+            i++;
+        }
+
+        return (int)(sign * result);
+    }
+}
